88b15449c4e9f317c4292ad5a5321badbee0b02a
Andrew McDavid <andrew.n.mcdavid@gmail.com>
2019-06-21 00:14:47 -0400

    Permutation method uses ccdb
    Need to add method to repeat a function on various canonicalizations of the cell_tbl

--- R/permutation-testing.R
+++ R/permutation-testing.R
@@ -1,49 +1,92 @@
-#' Tests for independence between clusters and labels using permutation
+#' Calculate number of cluster-subject singletons
 #'
-#' This tests a statistic for association between clusters and labels by permuting the link between the two
-#' Both clusters and labels should be factors.  Each observation represents a cell.
-#' For example, \code{clusters} could be a clonal ID, while \code{labels} is a subject ID.
-#' \code{statistic} is any
+#' @param cluster_idx factor-like cluster variable
+#' @param subject factor-like subject
 #'
-#' @param clusters \code{factor} of length n
-#' @param labels \code{factor} of length n
-#' @param statistic function of \code{clusters}, \code{labels} and optionally \code{covariates} that returns a \code{numeric} of length 1.
+#' @return average number of singletons
+#' @export
+#'
+#' @seealso cluster_permute_test
+purity = function(cluster_idx, subject) {
+    n_label_cluster = dplyr::bind_cols(cluster_idx = cluster_idx, subject = subject) %>%
+        group_by(cluster_idx, subject) %>% summarize(n = n()) %>% ungroup()
+    #Average number of singleton clusters for each subject
+    singletons = mean(n_label_cluster$n == 1)
+    singletons
+}
+
+
+#' Tests for independence between labels and covariates using permutation of cells
+#'
+#' This tests a statistic for association between labels (for instance, cluster/clonal ID) and covariates (for instance, subject or treatment) by permuting the link between the two.
+#' Each observation represents a cell.
+#' \code{statistic} is any function of `labels`
+#'
+#' @param ccdb `ContigCellDB`
+#' @param cell_covariate_keys  `character` naming fields in `ccdb$cell_tbl`
+#' @param cell_label_key `character` naming a single field in `ccdb$cell_tbl`
+#' @param statistic function of \code{covariates} (a `data.frame`), \code{labels} (a `vector`) that returns a \code{numeric} of length 1.
 #' @param n_perm number of permutations.
 #' @param ... passed to \code{statistic}
-#' @param covariates optional \code{factor} of length n.
 #' @param alternative `character` naming the direction `statistic` should be fall under the alternative hypothesis
 #'
 #' @return a list containing the observed value of the statistic, its expectation (under independence), a p-value, and the monte carlo standard error (of the expected value).
+#' @seealso purity
 #' @export
 #'
 #' @examples
 #' library(dplyr)
-#' purity = function(clusters, labels, covariates){
-#' n_label_cluster = data_frame(labels = labels, clusters = clusters) %>%
-#'     group_by(clusters, labels) %>% summarize(n = n()) %>% ungroup()
+#' # covariate must be a vector, rather than a data.frame
+#'
+#' cluster_idx = c(1, 1, 1, 2, 2, 3, 3)
+#' subject = c('A', 'A', 'B', 'B', 'B', 'C', 'C')
+#' contig_tbl = tibble(contig_pk = seq_along(cluster_idx), cluster_idx, subject)
+#' ccdb_test = ContigCellDB(contig_tbl = contig_tbl, contig_pk = 'contig_pk',
+#' cell_pk = c('contig_pk', 'subject', 'cluster_idx'), cluster_pk = 'cluster_idx')
+#' ccdb_test$cell_tbl
+#'
+#' purity_ex = function(cluster_idx, subject) {
+#' n_label_cluster = tibble(cluster_idx = cluster_idx, subject = subject) %>%
+#' group_by(cluster_idx, subject) %>% summarize(n = n()) %>% ungroup()
+#' #Average number of singleton clusters for each subject
 #' singletons = mean(n_label_cluster$n == 1)
 #' singletons
 #' }
+
+#' cluster_permute_test(ccdb_test, 'subject', 'cluster_idx',
+#' statistic = purity, n_perm  = 50)
 #'
-#' clusters = c(1, 1, 1, 2, 2, 3, 3)
-#' labels = c('A', 'A', 'B', 'B', 'B', 'C', 'C')
-#' covariates = c('X', 'X', 'Y', 'Y', 'Y', 'Y', 'Y')
-#' cluster_permute_test(clusters, labels, statistic = purity, n_perm  = 50)
+
+cluster_permute_test = function(ccdb, cell_covariate_keys,
+                                cell_label_key = ccdb$cluster_pk, statistic,
+                                n_perm,
+                                alternative = c("two.sided", "less","greater"),
+                                ...){
+    if(!is.character(cell_label_key) || length(cell_label_key) > 1) stop('`label_key` must name a single column in ccdb$cell_tbl')
+    label = ccdb$cell_tbl[[cell_label_key]]
+    if( length(na <- which(is.na(label))) > 0){
+        warning('Excluding ', length(na), ' cells with missing labels.')
+    }
+    covariates = ccdb$cell_tbl[cell_covariate_keys]
+    .cluster_permute_test(label, covariates, statistic, n_perm, alternative, ...)
+}
+
+
+#' Cell permutation tests (internal)
 #'
-cluster_permute_test = function(clusters, labels, covariates, statistic, n_perm = 1e3, alternative = c("two.sided", "less",
-                                                                                                       "greater"),  ...){
+#' @param labels \code{factor} of length n
+#' @param covariates \code{factor} of length n
+#' @param statistic function of label and covariate
+#' @inheritParams cluster_permute_test
+.cluster_permute_test = function(labels, covariates, statistic, n_perm, alternative,  ...){
     permp= rep(NA, n_perm)
     alternative = match.arg(alternative[1], c("two.sided", "less", "greater"), several.ok = FALSE)
 
     # checck that covariate nested within label
-    if(!missing(covariates)){
-        observed = statistic(clusters, labels, covariates, ...)
-    } else{
-        observed = statistic(clusters, labels, ...)
-    }
+    observed = statistic(labels, covariates, ...)
     for(i in seq_len(n_perm)){
-        ci = sample(clusters)
-        permp[i] = if(!missing(covariates)) statistic(ci, labels, covariates,  ...) else statistic(ci, labels, covariates)
+        ci = sample(labels)
+        permp[i] = statistic(ci, covariates, ...)
     }
     if(alternative == 'two.sided'){
         p_bound = min(mean(observed<permp), mean(observed>permp))*2
