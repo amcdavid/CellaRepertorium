72331ff1ed4f8272b25f30992b9adb4a4782e5b8
Andrew McDavid <andrew.n.mcdavid@gmail.com>
2020-10-06 01:56:54 -0400

    Check class with `inherits` rather than `class` and add an example

--- R/ccdb_join.R
+++ R/ccdb_join.R
@@ -1,28 +1,33 @@
 #' Join dataframe or SingleCellExperiment object with ContigCellDB object
 #'
-#' @param template Dataframe or SingleCellExperiment object to be joined with ccdb.
+#' @param template data.frame or SingleCellExperiment object to be joined with ccdb.
 #' @param ccdb A ContigCellDB object.
 #' @param join_fun Function used for the join operation.
 #' @param by A character vector of variables to join by.
 #'
 #' @return [ContigCellDB()]
 #' @export
-#'
+#' @examples
+#' data(ccdb_ex)
+#' to_join = dplyr::bind_rows(ccdb_ex$cell_tbl[1:10,],
+#' dplyr::tibble(barcode = c('extra1', 'extra2'), sample = LETTERS[1:2],
+#' pop = LETTERS[1:2]))
+#' ccdb_join(to_join, ccdb_ex)
 ccdb_join = function(template, ccdb, join_fun = dplyr::left_join, by = ccdb$cell_pk){
-    if(class(ccdb) == "CellaRepertorium") stop('ccdb must have class CellaRepertorium')
-    
+    if(!inherits(ccdb,  "ContigCellDB")) stop('ccdb must have class CellaRepertorium')
+
     #join
-    if(class(template) == "SingleCellExperiment") {
+    if(inherits(template,  "SingleCellExperiment")) {
         #check if all keys ccdb keys are in template
         if(!all(by %in% colnames(SingleCellExperiment::colData(template)))) stop('Not all ccdb keys present in template')
         ccdb$cell_tbl = join_fun(as.data.frame(SingleCellExperiment::colData(template)), ccdb$cell_tbl, by = by)
     }
-    else if(class(template) == "data.frame") {
+    else if(inherits(template, "data.frame") || inherits(template, "DataFrame")) {
         #check if all keys ccdb keys are in template
         if(!all(by %in% colnames(template))) stop('Not all ccdb keys present in template')
-        ccdb$cell_tbl = join_fun(template, ccdb$cell_tbl, by = by)
+        ccdb$cell_tbl = join_fun(template, as.data.frame(ccdb$cell_tbl), by = by)
     }
-    else stop('Template must have class SingleCellExperiment or data.frame')
-    
+    else stop('Template must inherit from `SingleCellExperiment` or `data.frame`')
+
     return(ccdb)
 }
