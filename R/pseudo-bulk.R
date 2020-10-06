698d1fbe471847c76d5cb977b0383b3b5611c04e
Andrew McDavid <andrew.n.mcdavid@gmail.com>
2020-10-06 00:42:41 -0400

    add example to generate_pseudobulk

--- R/pseudo-bulk.R
+++ R/pseudo-bulk.R
@@ -1,24 +1,37 @@
 #' Generate "pseudobulk" data from a `ContigCellDB`
 #'
+#' Tabulate contigs with a unique combination of `class_keys` per `total_keys`.
+#' For instance, `total_keys` might be a sample identifier, and `class_keys` might
+#' be the V- and J- gene identities.  The idea is that this might mimic the data
+#' generated in a bulk experiment.
+#'
+#' This function is currently rather 10x-specific, in that it is assumed that columns `barcode` and
+#' `umis` exist.
 #' @param ccdb [ContigCellDB()]
 #' @param class_keys `character` naming fields in `contig_tbl` that define unique classes of the repertoire
-#' @param total_keys `character` naming fields that also define a class, and moreover will be conditioned upon when calculating the total
+#' @param total_keys `character` naming fields to be conditioned upon when calculating the total.
 #' @param type one of "cell" or "umi"
 #'
 #' @return `tibble`
 #' @export
-#'
+#' @examples
+#' data(ccdb_ex)
+#' ccdb_ex = cluster_germline(ccdb_ex)
+#' pseudo = generate_pseudobulk(ccdb_ex, c('v_gene', 'j_gene', 'chain'), c('pop', 'sample'))
 generate_pseudobulk = function(ccdb, class_keys, total_keys, type = c('cell', 'umi')){
     type = match.arg(type, c('cell', 'umi'))
     all_keys = unique(c(class_keys, total_keys))
-
+    total_keys = setdiff(total_keys, class_keys)
+    if(!all(c('umis', 'barcode') %in% names(ccdb$contig_tbl))) stop("Expecting columns `barcode` and `umis` in `contig_tbl`.")
+    contig_grp = ccdb$contig_tbl %>% group_by(!!!syms(all_keys))
     if(type == 'umi'){
-        bulk <- ccdb$contig_tbl %>% group_by(!!!syms(all_keys)) %>% summarize(n_class = sum(umis))
+        bulk = contig_grp %>% summarize(n_class = sum(umis))
     } else {
-        bulk <- ccdb$contig_tbl %>% group_by(!!!syms(all_keys)) %>% summarize(n_class = dplyr::n_distinct(barcode))
+        bulk = contig_grp %>% summarize(n_class = dplyr::n_distinct(barcode))
     }
 
-    bulk = bulk %>% ungroup() %>% tidyr::complete(.,!!!syms(total_keys), tidyr::nesting(!!!syms(class_keys)),fill=list(n_class=0))
-    bulk <- bulk %>% group_by(!!!syms(total_keys)) %>% mutate(total = sum(n_class))
+    bulk = ungroup(bulk)
+    bulk = tidyr::complete(bulk,!!!syms(total_keys), tidyr::nesting(!!!syms(class_keys)),fill=list(n_class=0))
+    bulk = group_by(bulk, !!!syms(total_keys)) %>% mutate(total = sum(n_class))
 }
 
